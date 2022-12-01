d7274a7d20d1c23f8a46ee2042dd01bacc3e8d8a
Floxxyj <ije.cont3@gmail.com>
2022-12-01 11:17:17 -0800

    love

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,21 @@
-#!/usr/bin/env python3
-from magic_calculation_102 import add, sub
+#!/usr/bin/python3
+import sys
 
+if __name__ == '__main__':
+    """Prints the argument list passed to the program
+    The program takes all the arguments starting from the second
+    and prints the number of arguments and their value
+    """
+    av = sys.argv
+    l_av = len(av) - 1
 
-def magic_calculation(a, b):
-    if (a < b):
-        c = add(a, b)
-
-        for i in range(4, 6):
-            c = add(c, i)
-
-        return c
-    else:
-        return sub(a, b)
+    if l_av > 1:
+        print(l_av, 'arguments:')
+        for i in range(1, l_av + 1):
+            print('{:d}: {}'.format(i, av[i]))
+    elif l_av == 1:
+        print(l_av, 'argument:')
+        for i in range(1, l_av + 1):
+            print('{:d}: {}'.format(i, av[i]))
+    elif l_av == 0:
+        print(l_av, 'arguments.')
